8d1a7106550626df3c16e90c0f07752c84bd6b7d
Huann Almeida <huannvictor@gmail.com>
2022-07-13 22:09:53 -0300

    .

--- js/main.js
+++ js/main.js
@@ -8,12 +8,19 @@ elementsList.map((element, key) => {
   let figcaption = document.createElement("figcaption");
   let productTitle = document.createElement("p");
   let productPrice = document.createElement("p");
+
+  figure.classList.add("product-card");
+  img.classList.add("product-image");
+  figcaption.classList.add("product-description");
+  productTitle.classList.add("product-title");
+  productPrice.classList.add("product-price");
+
   img.src = `assets/${element.src}`;
   productTitle.innerText = `${element.title}`;
-  productPrice.innerText = `R$ ${element.price}`;
+  productPrice.innerHTML = `R$ <span>${element.price}</span> `;
+
   figure.append(img);
   figcaption.append(productTitle, productPrice);
   figure.append(figcaption);
   productList.append(figure);
-  console.log(element, key);
 });
